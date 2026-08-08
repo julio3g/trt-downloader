59651cdc8d9765921b45ecfb34fd3ac01d2631c9
Julio Cesar <juliocesar.orso@gmail.com>
2026-08-08 09:38:49 -0300

    🐛 fix: point tray icon to existing asset file

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -2,7 +2,10 @@ import { BrowserWindow, Menu, Tray } from 'electron'
 import { resolve } from 'node:path'
 
 export function createTray(window: BrowserWindow) {
-  const icon = '../../out/resources/icon.png' || '../../resources/icon.png'
+  const icon = '../../out/resources/deno.jpeg' || '../../resources/deno.jpeg'
+  if (!icon) {
+    throw new Error('Icon file not found')
+  }
   const tray = new Tray(resolve(__dirname, icon))
   const contextMenu = Menu.buildFromTemplate([
     {
